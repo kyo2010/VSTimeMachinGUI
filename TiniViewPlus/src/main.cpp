b054b894b0a6bc0266eaebbd7f9f42827e05979b
kyo <kyo@172.17.174.177>
2020-01-16 21:39:56 +0300

    Add Tini View Plus

--- TiniViewPlus/src/main.cpp
+++ TiniViewPlus/src/main.cpp
@@ -4,19 +4,13 @@
 #include <locale>
 
 //========================================================================
-int main() { 
-    //setlocale(LC_ALL, "RUS");
-    //setlocale(LC_CTYPE, "rus");
-    SetConsoleCP(CP_UTF8); 
-    //SetConsoleOutputCP(CP_UTF8);
-    //std::locale("rus_rus.1251");
-    //SetConsoleCP(CP_UTF8);
-    //SetConsoleOutputCP(CP_UTF8);
+int main() {
+
+#ifdef TARGET_WIN32
+    SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
     setlocale(LC_ALL, "Russian");
-
-    ofLogNotice() << "Запуск";
-    wprintf(L"%s", L"Unicode -- English -- Русский -- Ελληνικά -- Español.\n");
+#endif
 
 
     ofSetupOpenGL(1280, 720, OF_WINDOW);
